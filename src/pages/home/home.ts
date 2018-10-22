c6c0ef945c27bf2350f6d7c89d50d88b551f5d74
fergil <guerragil.fernanda@gmail.com>
2018-10-22 19:48:01 +0200

    New Functions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  vid = "https://www.youtube.com/embed/lRrkaLTVHrs";
+  constructor(public navCtrl: NavController, private dom:DomSanitizer) {
 
   }
-
+sanitize(vid){
+  return this.dom.bypassSecurityTrustResourceUrl(vid);
+}
 }
